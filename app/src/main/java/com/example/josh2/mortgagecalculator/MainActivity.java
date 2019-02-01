4990cf6972b74d3ede180aaac354608b51aa8827
Josh Garcia <josh2397garcia@gmail.com>
2019-02-01 03:19:38 -0500

    functionality for mortgage formula

--- app/src/main/java/com/example/josh2/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/josh2/mortgagecalculator/MainActivity.java
@@ -6,12 +6,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button calcButton;
     Button settingsButton;
     Button detailsButton;
+
+    TextView mortPrinET;
+    TextView interestET;
+    TextView amortET;
+
+    int num;
+
+    /* double prinDouble;
+    double interestDouble;
+    double numPayments; */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +33,26 @@ public class MainActivity extends AppCompatActivity {
         final int applyVal = applyIntent.getIntExtra("applyIntent", 0);
         //setTextSettings(applyVal);
 
+        mortPrinET = findViewById(R.id.mortPrinET);
+        interestET = findViewById(R.id.interestET);
+        amortET = findViewById(R.id.amortET);
+
+
+
         calcButton = findViewById(R.id.calcButton);
         calcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Log.i("final Setting number: ", Integer.toString(applyVal));
+                final double prinDouble = getPrinAmt();
+                final double interestDouble = getIntRateAmt() / determinePayFreq(applyVal);
+                final double numPayments = getPaymentsAmt() * determinePayFreq(applyVal);
+                //Log.i("PrinAmt., int, numPay: ", String.valueOf(prinDouble) + ", " + String.valueOf(interestDouble) + ", " + String.valueOf(numPayments));
+                double mortgage = calcMortgage(prinDouble, interestDouble, numPayments);
                 Intent summaryIntent = new Intent(getApplicationContext(), SummaryActivity.class);
                 summaryIntent.putExtra("applyVal", applyVal);
+                summaryIntent.putExtra("mortgageIntent", mortgage);
                 startActivity(summaryIntent);
+                clearEditTexts();
             }
         });
 
@@ -52,9 +75,63 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /*private void setTextSettings(int settingVal) {
-        if(settingVal == 0000) {
+    private void clearEditTexts() {
+        mortPrinET.setText("");
+        interestET.setText("");
+        amortET.setText("");
+    }
 
+    private double getPrinAmt() {
+        String principalAmt = mortPrinET.getText().toString();
+        double prinDouble = getPrinValue(principalAmt);
+        return prinDouble;
+    }
+
+    private double getIntRateAmt() {
+        String intRate = interestET.getText().toString();
+        double interestDouble = getIntRateAndPayments(intRate);
+        return interestDouble/100;
+    }
+
+    private double getPaymentsAmt() {
+        String amortAmt = amortET.getText().toString();
+        double numPayments = getIntRateAndPayments(amortAmt);
+        return numPayments;
+    }
+
+    private double determinePayFreq(int val) {
+        if((val == 0) || (val == 3) || (val == 6)) {
+            // bi-weekly
+            num = 24;
+        } else if((val == 1) || (val == 4) || (val == 7)) {
+            // weekly
+            num = 48;
+        } else if((val == 2) || (val == 5) || (val == 8)) {
+            // monthly
+            num = 12;
         }
-    }*/
+        return num;
+    }
+
+    private double getPrinValue(String string) {
+        try {
+            return Double.valueOf(string);
+        } catch(NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    private double getIntRateAndPayments(String string) {
+        try {
+            return Double.valueOf(string);
+        } catch(NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    private double calcMortgage(double principalAmt, double intRate, double amortPeriod) {
+        double mortgageAmt;
+        mortgageAmt = principalAmt * (((intRate * (Math.pow((1 + intRate), amortPeriod)))) / (Math.pow((1 + intRate), amortPeriod) - 1));
+        return mortgageAmt;
+    }
 }
--- app/src/main/java/com/example/josh2/mortgagecalculator/SummaryActivity.java
+++ app/src/main/java/com/example/josh2/mortgagecalculator/SummaryActivity.java
@@ -9,26 +9,58 @@ import android.widget.TextView;
 public class SummaryActivity extends AppCompatActivity {
 
     int settings;
+    double mortgage;
 
     TextView amountTextView;
-    TextView currencyTextView;
-    TextView paymentTextView;
+    //TextView currencyTextView;
+    //TextView paymentTextView;
+    TextView messageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
 
+        messageTextView = findViewById(R.id.messageTextView);
         amountTextView = findViewById(R.id.amountTextView);
-        currencyTextView = findViewById(R.id.currencyTextView);
-        paymentTextView = findViewById(R.id.paymentTextView);
+        //currencyTextView = findViewById(R.id.currencyTextView);
+        //paymentTextView = findViewById(R.id.paymentTextView);
 
         Intent settingIntent = getIntent();
         settings = settingIntent.getIntExtra("applyVal", 0);
-        setTextSettings(settings);
+        mortgage = settingIntent.getDoubleExtra("mortgageIntent", 0);
+        //setTextSettings(settings);
+
+        if((settings == 0) || (settings == 3) || (settings == 6)) {
+            // bi-weekly
+            messageTextView.setText("To pay your mortgage, you should make bi-weekly payments of: ");
+            messageTextView.setVisibility(View.VISIBLE);
+        } else if((settings == 1) || (settings == 4) || (settings == 7)) {
+            // weekly
+            messageTextView.setText("To pay your mortgage, you should make weekly payments of: ");
+            messageTextView.setVisibility(View.VISIBLE);
+        } else if((settings == 2) || (settings == 5) || (settings == 8)) {
+            // monthly
+            messageTextView.setText("To pay your mortgage, you should make monthly payments of: ");
+            messageTextView.setVisibility(View.VISIBLE);
+        }
+
+        if((settings == 0) || (settings == 1) || (settings == 2)) {
+            // dollar
+            amountTextView.setText("$ " + String.format("%.2f", mortgage));
+            amountTextView.setVisibility(View.VISIBLE);
+        } else if((settings == 3) || (settings == 4) || (settings == 5)) {
+            // euro
+            amountTextView.setText("€ " + String.format("%.2f", mortgage));
+            amountTextView.setVisibility(View.VISIBLE);
+        } else if((settings == 6) || (settings == 7) || (settings == 8)) {
+            // pound
+            amountTextView.setText("£ " + String.format("%.2f", mortgage));
+            amountTextView.setVisibility(View.VISIBLE);
+        }
     }
 
-    private void setTextSettings(int settingVal) {
+    /*private void setTextSettings(int settingVal) {
         if(settingVal == 0) {
             currencyTextView.setText("$ DOLLAR");
             paymentTextView.setText("biweekly payment");
@@ -75,5 +107,5 @@ public class SummaryActivity extends AppCompatActivity {
             currencyTextView.setVisibility(View.VISIBLE);
             paymentTextView.setVisibility(View.VISIBLE);
         }
-    }
+    }*/
 }
